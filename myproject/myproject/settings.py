4427166eee36c43339447bf7412f932ee1a7812e
Wayne Yamamoto <wayne.k.yamamoto@gmail.com>
2012-09-05 00:49:17 +0000

    got it to rum at webfaction

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'myproject_db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/uwcse403/myproject_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -56,11 +56,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/uwcse403/webapps/static/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://uwcse403.webfactional.com/static/static'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
